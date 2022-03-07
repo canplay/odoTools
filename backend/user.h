0def44888fe081dd5dc50964b4f559106a4cb091
CaNplay <canplay@vip.qq.com>
2022-03-07 17:52:02 +0800

    #1

--- backend/user.h
+++ backend/user.h
@@ -13,12 +13,14 @@ namespace api
 		public:
 			METHOD_LIST_BEGIN
 				METHOD_ADD(User::reg, "/register", Post);
+			METHOD_ADD(User::login, "/login", Post);
 			METHOD_LIST_END
 
 		public:
 			User();
 
 			void reg(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback) const;
+			void login(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback) const;
 		};
 	}
 }
